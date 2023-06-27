fe2e247adccc49b9771299c13cd18c23fe8a5357
Folgui12 <folguerasbenjamin@gmail.com>
2023-06-27 16:27:59 -0300

    Nearly Finished

---
+++ Assets/NewGame/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] int seg, min;
+    [SerializeField] Text reloj;
+
+    private float restante;
+
+    public Walter_Open_World walter;
+
+    void Awake()
+    {
+        restante = (min * 60) + seg;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        restante -= Time.deltaTime; 
+        if(restante < 1 && walter.tries > 0)
+        {
+            // Pantalla de Victoria
+        }
+
+        int tempMin = Mathf.FloorToInt(restante / 60);
+        int tempSeg = Mathf.FloorToInt(restante % 60);
+
+        reloj.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
+    }
+}
--- Assets/NewGame/Scripts/Walter/Walter_Open_World.cs
+++ Assets/NewGame/Scripts/Walter/Walter_Open_World.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Walter_Open_World : MonoBehaviour
 {
+    public ParticleSystem smoke1;
+    public ParticleSystem smoke2;
     public float speed = 20;
 
     private bool canBeHit = true;
@@ -23,11 +25,18 @@ public class Walter_Open_World : MonoBehaviour
 
     private Animator anim;
 
+    public Transform spawnPoint;
+
+    public int tries = 3;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        smoke1.Stop();
+        smoke2.Stop();
     }
 
     // Update is called once per frame
@@ -48,12 +57,10 @@ public class Walter_Open_World : MonoBehaviour
             {
                 imBoosted = false;
                 boostCounter = 0f;
-                speed = 18; 
+                speed = 17; 
             }
         }
-
-        if(life <= 0) Debug.Log("Perdiste");
-
+    
         transform.Translate(carMovement);
 
         Debug.Log(life);
@@ -80,8 +87,6 @@ public class Walter_Open_World : MonoBehaviour
             speed = 5;
             onWater = true;
         }
-
-
 
     }
 
@@ -113,5 +118,26 @@ public class Walter_Open_World : MonoBehaviour
     public void CantBeHit()
     {
         canBeHit = false;
+    }
+
+    public void lifeController()
+    {
+        if(life < 5 && life > 2)
+        {
+            smoke1.Play();
+        }
+        else if (life < 2)
+        {
+            smoke1.Stop();
+            smoke2.Play();
+        }
+        if(life <= 0) 
+        {
+            life = 5;
+            transform.position = spawnPoint.position;
+            smoke2.Stop();
+            tries--;
+            if(tries == 0) {} // Pantalla de Derrota
+        }
     }
 }
